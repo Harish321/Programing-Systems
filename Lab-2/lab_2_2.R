ca7f17d743552042cf1b005320ad29f01ade213a
Harish Dhulipalla <harish.d14@iiits.in>
2018-03-12 14:51:07 +0530

    lab 2 update

--- Lab-2/lab_2_2.R
+++ Lab-2/lab_2_2.R
@@ -1,6 +1,7 @@
 tabel <-read.csv(file="dataset.csv");
 generation <- c(0,0,0,0)
-for (item in 5:nrow(tabel)){
+allrelations <- c()
+for (item in 5:nrow(tabel) ){
   related_person <- tabel$Relatedto[[item]];
   related_person <- as.numeric(as.character(related_person))
   relation <- tabel$Relation[item]
@@ -38,7 +39,7 @@ generation0 <- function(a,b,gt_a,gt_b,d){
   else{
   tempa <- tabel$Relatedto[[a]]
   tempa <- as.numeric(as.character(tempa))
-  print (tempa)
+  # print (tempa)
   tempb <- tabel$Relatedto[[b]]
   tempb <- as.numeric(as.character(tempb))
   gt_tempa <- generation[[tempa]]
@@ -137,10 +138,10 @@ generation3 <- function (a,b,gt_a,gt_b,d){
   if (d == 3){
     temp <- tabel$Relatedto[[a]]
     temp <- as.numeric(as.character(temp))
-    print (temp)
+    # print (temp)
     gt_temp <- generation[[temp]]
-    print (gt_temp)
-    print (gt_b)
+    # print (gt_temp)
+    # print (gt_b)
     if(gt_temp - gt_b == 2){
       sec_gen_rel <- generation2(temp,b,gt_temp,gt_b,2)
       return (sec_gen_rel)
@@ -159,22 +160,23 @@ generation3 <- function (a,b,gt_a,gt_b,d){
       else{return(5)}
     }
 }
-i <- 1
-while (i==1){
+Name1 <- readline(prompt="Name:")
+Name2 <- readline(prompt="Relation:")
+for (names in 1:nrow(tabel)){
     # print (name1)
-Name1 <- readline(prompt="Name1:")
-Name2 <- readline(prompt="Name2:")
+# Name1 <- readline(prompt="Name1:")
+# Name2 <- readline(prompt="Name2:")
 
-p1 <- subset(tabel$PKey,tabel$Name == Name1)
-p2 <- subset(tabel$PKey,tabel$Name == Name2)
-p1 <- as.numeric(as.character(p1))
+p2 <- subset(tabel$PKey,tabel$Name == Name1)
+# p2 <- subset(tabel$PKey,tabel$Name == Name2)
 p2 <- as.numeric(as.character(p2))
+# p2 <- as.numeric(as.character(p2))
 # # print (p1
 # print (p2)
 # p1 <- 1
 # p1 <- as.numeric(p1)
-# p2 <- names
-# p2 <- as.numeric(p2)
+p1 <- names
+p1 <- as.numeric(p1)
 # print (p2)
 gt_p1 <- generation[[p1]]
 gt_p2 <- generation[[p2]]
@@ -182,81 +184,97 @@ gender <- tabel$Gender[[p1]]
 gender <- as.character(gender)
 diff_gen <- gt_p1 - gt_p2
 # print (diff_gen)
+if (p1 != p2){
 if(diff_gen == 0){
   realation <- generation0(p1,p2,gt_p1,gt_p2,diff_gen)
   if(realation == 6){
-    if(gender == "M"){print("P1 is Brother to P2")}
-    else{print("P1 is sister to P2")}
+    if(gender == "M"){allrelations <- c(allrelations,'Brother')}
+    else{allrelations <- c(allrelations,'Sister')}
   }
   if(realation == 7){
-    if(gender == "M"){print("P1 is Cousin to P2")}
-    else{print("P1 is Cousin to P2")}
+    if(gender == "M"){allrelations <- c(allrelations,'Cousin')}
+    else{allrelations <- c(allrelations,'Cousin')}
   }
   if(realation == 8){
-    if(gender == "M"){print("P1 is Husband of P2")}
-    else{print("P1 is wife of P2")}
+    if(gender == "M"){allrelations <- c(allrelations,'Husband')}
+    else{allrelations <- c(allrelations,'Wife')}
   }
   if(realation == 9){
-    print("its a complicated relation between p1 and p2")
+    allrelations <- c(allrelations,'Complicated')
   }
 }
 if( diff_gen == 1 | diff_gen == -1){
  realation <- generation1(p1,p2,gt_p1,gt_p2,diff_gen)
  if (realation == 0){
-    if(gender == "M"){print ("P1 is Son to P2")}
-    else{print("P1 is Daughter of P2")} 
+    if(gender == "M"){allrelations <- c(allrelations,'Son')}
+    else{allrelations <- c(allrelations,'Daughter')} 
   }
   if (realation == 1){
-    if(gender == "M"){print ("P1 is Son-in-law to P2")}
-    else{print("P1 is Daughter-in-law of P2")} 
+    if(gender == "M"){allrelations <- c(allrelations,'Son-in-law')}
+    else{allrelations <- c(allrelations,'Daughter-in-law')} 
   }
   if (realation == 2){
-    if(gender == "M"){print ("P1 is Father to P2")}
-    else{print("P1 is Mother of P2")} 
+    if(gender == "M"){allrelations <- c(allrelations,'Father')}
+    else{allrelations <- c(allrelations,'Mother')} 
   }
   if (realation == 3){
-    if(gender == "M"){print ("P1 is Father-in-law to P2")}
-    else{print("P1 is Mother-in-law of P2")} 
+    if(gender == "M"){allrelations <- c(allrelations,'Father-in-law')}
+    else{allrelations <- c(allrelations,'Mother-in-law')} 
   }
   if (realation == 4){
-    if(gender == "M"){print ("P1 is Nephew to P2")}
-    else{print("P1 is Neice of P2")} 
+    if(gender == "M"){allrelations <- c(allrelations,'Nephew')}
+    else{allrelations <- c(allrelations,'Neice')} 
   }
   if (realation == 5){
-    if(gender == "M"){print ("P1 is Uncle to P2")}
-    else{print("P1 is Aunty of P2")} 
+    if(gender == "M"){allrelations <- c(allrelations,'Uncle')}
+    else{allrelations <- c(allrelations,'Aunty')} 
   }
 }
 if(diff_gen == 2 | diff_gen == -2){
   realation <- generation2(p1,p2,gt_p1,gt_p2,diff_gen)
   if (realation == 0| realation ==1|realation ==4){
-    if(gender == "M"){print ("P1 is Grand Son to P2")}
-    else{print("P1 is Grand Daughter of P2")} 
+    if(gender == "M"){allrelations <- c(allrelations,'Grandson')}
+    else{allrelations <- c(allrelations,'Granddaughter')} 
   }
   if (realation == 2 | realation ==3){
-    if(gender == "M"){print ("P1 is Grand Father to P2")}
-    else{print("P1 is Grand Mother of P2")} 
+    if(gender == "M"){allrelations <- c(allrelations,'Grandfather')}
+    else{allrelations <- c(allrelations,'Grandmother')} 
   }
   if (realation == 5){
-    if(gender == "M"){print ("P1 is Grand Uncle to P2")}
-    else{print("P1 is Grand Aunty of P2")} 
+    if(gender == "M"){allrelations <- c(allrelations,'Granduncle')}
+    else{allrelations <- c(allrelations,'Grandaunty')} 
   }
 }
 if( diff_gen == 3 | diff_gen == -3){
   realation <- generation3(p1,p2,gt_p1,gt_p2,diff_gen)
   if (realation == 0| realation ==1|realation ==4){
-    if(gender == "M"){print ("P1 is Great Grand Son to P2")}
-    else{print("P1 is Great Grand Daughter of P2")} 
+    if(gender == "M"){allrelations <- c(allrelations,'Greatgrandson')}
+    else{allrelations <- c(allrelations,'Greatgranddaughter')} 
   }
   if (realation == 2 | realation ==3){
-    if(gender == "M"){print ("P1 is Great Grand Father to P2")}
-    else{print("P1 is Great Grand Mother of P2")} 
+    if(gender == "M"){allrelations <- c(allrelations,'Greatgrandfather')}
+    else{allrelations <- c(allrelations,'Greatgrandmother')} 
   }
   if (realation == 5){
-    if(gender == "M"){print ("P1 is Great Grand Uncle to P2")}
-    else{print("P1 is Great Grand Aunty of P2")} 
+    if(gender == "M"){allrelations <- c(allrelations,'Greatgranduncle')}
+    else{allrelations <- c(allrelations,'Greatgrandaunty')} 
   }
 }
+}
+else {
+  allrelations <- c(allrelations,'Self')
+}
+}
+for (i in 1:nrow(tabel)){
+  current_relation <- allrelations[[i]]
+  current_relation <- as.character(current_relation)
+  # print (current_relation)
+  if(current_relation == Name2 )
+  {
+    p1 <- tabel$Name[i]
+    p1 <- as.character(p1)
+    print (p1)
+  }
 }
 
 
